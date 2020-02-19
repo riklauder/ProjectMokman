ae0f1ff4d2b25ce24151a9b8f1e702a66dec871f
EsteeRixx <riklauder@users.noreply.github.com>
2020-02-18 20:36:36 -0500

    sync

--- AIversion/projectParams.py
+++ AIversion/projectParams.py
@@ -10,5 +10,5 @@
 
 STUDENT_CODE_DEFAULT = 'multiAgents.py'
 PROJECT_TEST_CLASSES = 'multiagentTestClasses.py'
-PROJECT_NAME = 'Project 2: Multiagent search'
+PROJECT_NAME = 'Project Mukman'
 BONUS_PIC = False

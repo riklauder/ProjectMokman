636421987fe5fa2ce21ef5a15e83c1c7c5f67166
EsteeRixx <riklauder@hotmail.com>
2020-02-20 17:10:18 -0500

    sync - partial map transform complete

--- AIversion/graphicsDisplay.py
+++ AIversion/graphicsDisplay.py
@@ -258,7 +258,7 @@ class PacmanGraphics:
     begin_graphics(screen_width,
                    screen_height,
                    BACKGROUND_COLOR,
-                   "Pacman AI Framework")
+                   "Project Mukman")
 
   def drawPacman(self, pacman, index):
     position = self.getPosition(pacman)
---
+++ src/mapTransform.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <iomanip>
+#include <numeric>
+#include <cstdlib>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <limits>
+#include <iterator>
+
+void getData(std::vector<std::string>& data) {
+	std::string s1;
+	int rows=31;
+	int cols=28;
+	std::ifstream file("layouts/randomMap.lay");
+	getline(file, s1);
+	for (int i=0; i < rows;i++) {
+		std::string s2 ="";
+		if (i==0 || i == rows-1)
+			s2.append("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+		else{
+			s2.append("%");
+			s2.append(s1.substr((i*cols)+1, cols-2));
+			s2.append("%");	
+		}
+		data.push_back(s2);
+			}
+	
+	}
+
+
+
+int main(){
+	std::vector<std::string> map;
+
+	getData(map);
+
+	map[23][14]='P';
+	for (auto x : map)
+		std::cout << x << std::endl;
+
+}

ac40badc0ecaa815840ecd9f70585bd993a08f7f
EsteeRixx <riklauder@hotmail.com>
2020-03-23 12:42:00 -0400

    added .sh file for launch in linux

---
+++ src/mokman.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+@echo off
+node mapgen.js > layouts/randomMap0.lay
+node mapgen.js > layouts/randomMap1.lay
+node mapgen.js > layouts/randomMap2.lay
+node mapgen.js > layouts/randomMap3.lay
+node mapgen.js > layouts/randomMap4.lay
+node mapgen.js > layouts/randomMap5.lay
+node mapgen.js > layouts/randomMap6.lay
+node mapgen.js > layouts/randomMap7.lay
+node mapgen.js > layouts/randomMap8.lay
+node mapgen.js > layouts/randomMap9.lay
+node mapgen.js > layouts/randomMap10.lay
+node mapgen.js > layouts/randomMap11.lay
+node mapgen.js > layouts/randomMap12.lay
+
+bash -c ./mapt > layouts/randomfMap.lay
+python mokman.py
